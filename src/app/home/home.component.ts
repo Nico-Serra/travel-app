af9c383c5c03a1b4d02cb441afc4e44d6bb86c87
Nico-Serra <nico.serra000@gmail.com>
2024-08-01 12:32:01 +0200

    add component from header, home, footer and modal

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
+import { FormsModule } from '@angular/forms';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FormsModule, ModalComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  constructor(private localStorageService: LocalStorageService) { }
+
+  formInit = true;
+  mounth = "June";
+  dayOfMounth = 30;
+
+  saveMount() {
+    //console.log(this.mounth);
+    localStorage.setItem('mounth', this.mounth)
+    this.formInit = false;
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('mounth')) {
+      this.formInit = false;
+      this.mounth =  localStorage.getItem('mounth')!
+    }
+
+  }
+}
