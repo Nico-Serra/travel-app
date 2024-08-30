78c554d6cb969b855642d745009dff07c41b74c7
Nico-Serra <nico.serra000@gmail.com>
2024-08-30 11:05:47 +0200

    Add welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { SingleTripComponent } from "./single-trip/single-trip.component";
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent, SingleTripComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SingleTripComponent } from './single-trip/single-trip.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 
-export const routes: Routes = [{path:'', component:HomeComponent}, 
+export const routes: Routes = [
+     {path:'', component:WelcomePageComponent},
+     {path:'travel', component:HomeComponent}, 
      { path: 'travel/:place', component: SingleTripComponent },
      { path: 'error', component: ErrorPageComponent, data: { message: 'Page not found!' } },
-     { path: '**', redirectTo: 'error' }];
+     { path: '**', redirectTo: 'error' }
+];
