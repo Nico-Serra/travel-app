778dc73b9d985a03722d0c96ab74e244b4dbf073
Nico-Serra <nico.serra000@gmail.com>
2024-09-01 12:54:18 +0200

    fix angular,json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Icon } from 'leaflet';
+
+Icon.Default.imagePath = 'assets/leaflet/';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
