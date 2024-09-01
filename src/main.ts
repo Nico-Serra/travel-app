91126c3530955f5360deb19f60b8706cce5c6cf8
Nico-Serra <nico.serra000@gmail.com>
2024-09-01 18:26:19 +0200

    fix header after build

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { Icon } from 'leaflet';
 
-Icon.Default.imagePath = 'assets/leaflet/';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
